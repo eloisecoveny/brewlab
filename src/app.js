5985d07313c1e3fe455337675325efd0093ac493
Eloise Coveny <eloisecoveny@gmail.com>
2019-02-19 21:40:37 +0000

    MVP completed, first extension attempted.

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import Vue from "vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#app",
+    data: {
+      beers: [],
+      selectedBeerIndex: "",
+      selectedBeer: null,
+      favouriteBeers: []
+    },
+    mounted: function(){
+      this.getBeers();
+    },
+    methods: {
+      getBeers: function(){
+        fetch("https://api.punkapi.com/v2/beers")
+          .then(response => response.json())
+          .then(data => this.beers = data);
+      },
+      beerSelect: function(){
+        this.selectedBeer = this.beers[this.selectedBeerIndex];
+      },
+      addToFavourites: function(){
+        this.favouriteBeers.push(this.selectedBeer);
+      },
+      removeFromFavourites: function(index){
+        this.favouriteBeers.splice(index, 1);
+      }
+    }
+  })
+})
